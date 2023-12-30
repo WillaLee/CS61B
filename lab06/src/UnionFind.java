042f59e61b7e46a9ecd1285280c7ac65b6697704
Liwei <willalee0236@gmail.com>
2023-12-29 23:07:46 -0800

    Completes lab 6

--- lab06/src/UnionFind.java
+++ lab06/src/UnionFind.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class UnionFind {
     /**
      * DO NOT DELETE OR MODIFY THIS, OTHERWISE THE TESTS WILL NOT PASS.
@@ -9,34 +12,44 @@ public class UnionFind {
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
-        // TODO: YOUR CODE HERE
+        data = new int[N];
+        Arrays.fill(data, -1);
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        int parent = data[v];
+        while (parent >= 0) {
+            parent = data[parent];
+        }
+        return -parent;
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return data[v];
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO: YOUR CODE HERE
-        return false;
+        int root1 = find(v1);
+        int root2 = find(v2);
+        return root1 == root2;
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. If invalid items are passed into this
        function, throw an IllegalArgumentException. */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        if (v >= data.length){
+            throw new IllegalArgumentException();
+        }
+        int root = v;
+        while (data[root] >= 0){
+            root = data[root];
+        }
+        return root;
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -44,8 +57,20 @@ public class UnionFind {
        used. If the sizes of the sets are equal, tie break by connecting V1's
        root to V2's root. Union-ing a item with itself or items that are
        already connected should not change the structure. */
-    public void union(int v1, int v2) {
-        // TODO: YOUR CODE HERE
+    public void union(int  v1, int v2) {
+        if (v1 == v2) {return;}
+        int root1 = find(v1);
+        int root2 = find(v2);
+        if (data[root1] < data[root2]){
+            unionHelper(root1,root2);
+        }else {
+            unionHelper(root2,root1);
+        }
+    }
+
+    public void unionHelper(int heavyRoot, int lightRoot){
+        data[heavyRoot] = data[heavyRoot] + data[lightRoot];
+        data[lightRoot] = heavyRoot;
     }
 
     /**
